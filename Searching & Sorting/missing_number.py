0f26e66b0cbf3ff81b3718bb299a4d04366c4468
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-03-10 12:12:19 -0600

    GFG

--- Searching & Sorting/missing_number.py
+++ Searching & Sorting/missing_number.py
@@ -16,3 +16,17 @@ Output: 8
         
         n = len(nums)
         return n*(n+1)//2 - sum(nums)
+    
+    # Method 2: XOR - A^A = 0
+    '''
+    x1 = a[0] 
+    x2 = 1
+      
+    for i in range(1,n): 
+        x1 = x1 ^ a[i] 
+          
+    for i in range(2,n+2): 
+        x2 = x2^i 
+      
+    return x1 ^ x2 
+    '''
