6d7c8cfb3b5e0b2187a8594920a49bd82f98ce79
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-03-12 15:03:29 -0600

    700. Search in a Binary Search Tree

---
+++ Trees/search_tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+        """
+        
+        while root:
+            if val == root.val:
+                return root
+            elif val > root.val:
+                root = root.right
+            else:
+                root = root.left
+        return None
+            
+            
+        
