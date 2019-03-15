f548436dbc88fac69dd842da013355aac4f13998
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-03-14 23:05:44 -0600

    994. Rotting Oranges

---
+++ Graphs/rotten_oranges.py
@@ -0,0 +1,57 @@
+'''
+n a given grid, each cell can have one of three values:
+
+the value 0 representing an empty cell;
+the value 1 representing a fresh orange;
+the value 2 representing a rotten orange.
+Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
+
+ 
+
+Example 1:
+
+
+
+Input: [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+
+'''
+import collections
+class Solution(object):
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        R, C = len(grid), len(grid[0])
+
+        queue = collections.deque()
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                if val == 2:
+                    queue.append((r, c, 0))
+
+        def neighbors(r, c):
+            for nr, nc in ((r-1,c),(r,c-1),(r+1,c),(r,c+1)):
+                if 0 <= nr < R and 0 <= nc < C:
+                    yield nr, nc
+
+        d = 0
+        while queue:
+            r, c, d = queue.popleft()
+            for nr, nc in neighbors(r, c):
+                if grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    queue.append((nr, nc, d+1))
+
+        if any(1 in row for row in grid):
+            return -1
+        return d
+    
+                
+            
+            
+            
+                
