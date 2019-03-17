f2194330e87eb56bd2e0f949b5a0867a45b3406f
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-03-16 23:43:44 -0600

    617. Merge Two Binary Trees

--- Trees/merge_trees.py
+++ Trees/merge_trees.py
@@ -30,15 +30,12 @@ Merged tree:
         """
         
         if t1 and t2:
-            t1.val = t1.val + t2.val
-            t1.left = self.mergeTrees(t1.left,t2.left)
-            t1.right = self.mergeTrees(t1.right,t2.right)
-        
-        if t2 and not t1: 
-            t1 = t2
-        
+            t1.val += t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+        elif not t1:
+            return t2
         return t1
-            
         
         
         
