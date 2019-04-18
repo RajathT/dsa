33a6636b90fb3df126b85cc5eac7e18d1f982d11
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-04-18 12:04:38 -0600

    3. Longest Substring Without Repeating Characters

--- Arrays & Strings/longest_non_repeating_substring.py
+++ Arrays & Strings/longest_non_repeating_substring.py
@@ -1,10 +1,4 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        Example 1:
-
+"""
 Input: "abcabcbb"
 Output: 3 
 Explanation: The answer is "abc", with the length of 3. 
@@ -18,25 +12,33 @@ Example 3:
 Input: "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3. 
-             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+"""
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
         """
-        i, j, c, dm = 0, 0, 0, 0
-        d = {}
-        while j<len(s):
-            if s[j] not in d or d[s[j]] == 0:
-                d[s[j]] = 1
+        :type s: str
+        :rtype: int
+        """
+        begin, end, m, c, D = 0, 0, 0, 0, {}
+        
+        while end < len(s):
+            if s[end] not in D or D[s[end]] == 0:
+                D[s[end]] = 1
             else:
-                d[s[j]] += 1
-                c += 1
-            j += 1
-            while c > 0:
-                if d[s[i]] > 1:
+                D[s[end]] += 1
+                c = 1
+            end += 1
+            
+            while c:
+                if D[s[begin]] > 1:
                     c -= 1
-                d[s[i]] -= 1
-                i += 1
-            dm = max(dm, j-i)
-            #print(dm , j, i)
-        return dm
+                D[s[begin]] -= 1
+                begin += 1
+                
+            m = max(m, end-begin)//Keeps increasing even before entering while loop
+
+        return m
                 
                 
                 
