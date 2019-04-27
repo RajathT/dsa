7f6a1767d12bc035a4e209c32a8831dba7326e0a
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-04-27 17:24:51 -0600

    832. Flipping an Image

---
+++ java/Arrays/flipping_image.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int row_length = A[0].length;
+        for (int[] row: A){
+            for (int i=0; i<(row_length+1)/2; i++){
+                int temp = row[i] ^ 1;
+                row[i] = row[row_length-i-1] ^ 1;
+                row[row_length-i-1] = temp;
+            }
+        }
+        return A;
+    }
+}
