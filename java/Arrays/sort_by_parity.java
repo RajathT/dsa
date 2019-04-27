7dc6c87e1c022f7a2c22618f1f310dfc2f92cda3
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-04-27 17:22:02 -0600

    905. Sort Array By Parity

---
+++ java/Arrays/sort_by_parity.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int i = 0, j = A.length-1;
+        while (i < j){
+            while (i<A.length-1 && A[i]%2 == 0)
+                i ++;
+            while (j>=0 && A[j]%2 == 1)
+                j --;
+            
+            if (i < j){
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                i++;j--;
+            }
+        }
+        return A;
+    }
+}
