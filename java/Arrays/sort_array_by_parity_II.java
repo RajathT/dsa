bba4b24eb2236eadb77444afbb29d3ba6fa876c8
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-04-28 13:15:21 -0600

    Create sort_array_by_parity_II.java

---
+++ java/Arrays/sort_array_by_parity_II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int i = 0, j = 1;
+        while (i<A.length && j<A.length){
+            while (i<A.length && A[i]%2 == 0)
+                i += 2;
+            while (j<A.length && A[j]%2 == 1)
+                j += 2;
+            if (i<A.length && j<A.length){
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                i += 2; j += 2;
+            }
+        }
+        return A;
+    }
+}
