748321ccc89cfc89c85d91a9224927cad36f9b9a
Rajath Tellapuram <35347929+RajathT@users.noreply.github.com>
2019-05-22 19:17:38 -0600

    1. Two Sum

--- java/HashTable/two_sum.java
+++ java/HashTable/two_sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        int i=0;
-        for (; i<nums.length; i++){
-            if (map.containsKey(nums[i])) break;
-            else map.put(target-nums[i], i);
+        
+        for (int i=0; i<nums.length; i++){
+            if (map.containsKey(nums[i])) return new int[]{i, map.get(nums[i])};
+            else map.put(target - nums[i], i);
         }
-        return new int[]{i, map.get(nums[i])};
+        return new int[]{-1,-1};
     }
 }
